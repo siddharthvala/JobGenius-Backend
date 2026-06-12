e5a9d75d5a55e5f9a54db2ff811f894a57aa984c
siddharthvala <siddharthvala007@gmail.com>
2026-06-12 06:40:16 +0530

    Allow Vercel domain

--- src/main/java/com/jobmatcher/jobmatcher_backend/config/SecurityConfig.java
+++ src/main/java/com/jobmatcher/jobmatcher_backend/config/SecurityConfig.java
@@ -103,7 +103,7 @@ public class SecurityConfig {
 
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://job-genius-seven.vercel.app"));
+        config.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://jobgeniusvala.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
